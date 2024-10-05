d43e0e424b6334d2cfdf2ca8cfcdd78f9c8df324
Maeva Nono <maeva.nono@efrei.net>
2024-10-05 17:23:22 +0200

    Menu v2 + debut de developpement et integration des fonctions

--- project_script.sh
+++ project_script.sh
@@ -3,15 +3,22 @@
 #fonction2 : full_scan
 #fonction3 : custom_scan
 
-#fonction 4 : os_detect_on
-#fonction 4 : detection_system_off
-#fonction 5 : version_detect_on
-#fonction 6 : version_detect_off
+#fonction 4 : toggle_os_detect
+#fonction 5 : toggle_version_detect
 
 #fonction 6 :
 
+detect_os="off"
+detect_version="off"
+
 fast_scan(){
-    :
+    echo "Entrez une adresse ip ou une URL"
+    read user_scan
+    if [[ -n user_scan  ]]; then
+        nmap -F "$user_scan"
+    else 
+        echo "Le champs est vide"
+    fi
 }
 
 full_scan(){
@@ -22,20 +29,24 @@ custom_scan(){
     :
 }
 
-os_detect_on(){
-    :
+toggle_os_detect(){
+    if [[ $detect_os=="off" ]]; then
+        detect_os="on"
+        echo "Detection d'OS active."
+    else
+        detect_os="off"
+        echo "Detection d'OS desactive."
+    fi
 }
 
-os_detect_off(){
-    :
-}
-
-version_detect_on(){
-    :
-}
-
-version_detect_off(){
-    :
+toggle_version_detect(){
+    if [[ $detect_version=="off" ]]; then
+        detect_version="on"
+        echo "Detection des services et versions logiciel active."
+    else
+        detect_version="off"
+        echo "Detection des services et versions logiciel desactive."
+    fi
 }
 
 single_scan(){
@@ -50,65 +61,140 @@ multiple_scan(){
     :
 }
 
+sous_menu_scans(){
+    while true; do
+        #clear
+        echo "1. Faire un scan rapide"
+        echo "2. WIP"
+        echo "3. WIP"
+        echo "4. Back"
+        read -p "Choisissez une option : " choix_1
+        case $choix_1 in
+
+            1) 
+            clear
+            fast_scan #A Faire
+            ;;
+
+            2) 
+            clear
+            full_scan #A Faire
+            ;;
+
+            3) 
+            clear
+            custom_scan #A Faire
+            ;;
+
+            4) 
+            clear
+            break
+            ;;
+
+            *) 
+            clear
+            echo "Choix invalide. Essayez de nouveau."
+            ;;
+        esac
+    done
+}
+
+sous_menu_detection(){
+    while true; do
+        clear
+        echo "1. Activer/Desactiver la detection OS (actuellement $detect_os)"
+        echo "2. Activer/Desactiver la detectionn des services et logiciels (actuellement $detect_version)"
+        echo "3. Back"
+        read -p "Choisissez une option : " choix_1
+        case $choix_1 in
+
+            1) 
+            echo "WIP" #A Faire
+            ;;
+
+            2) 
+            echo "WIP" #A Faire
+            ;;
+
+            3) 
+            clear
+            break;;
+
+            *) 
+            clear
+            echo "Choix invalide. Essayez de nouveau."
+            ;;
+        esac
+    done
+}
+
+sous_menu_planification(){
+    while true; do
+        clear
+        echo "1. WIP"
+        echo "2. WIP"
+        echo "3. WIP"
+        echo "4. Back"
+        read -p "Choisissez une option : " choix_1
+        case $choix_1 in
+
+            1) 
+            echo "WIP" #A Faire
+            ;;
+
+            2) 
+            cho "WIP" #A Faire
+            ;;
+
+            3) 
+            echo "WIP" #A Faire
+            ;;
+
+            4) 
+            clear
+            break
+            ;;
+
+            *) 
+            clear
+            echo "Choix invalide. Essayez de nouveau."
+            ;;
+        esac
+    done
+}
+
 while true; do
     echo "1. Tous les scans"
     echo "2. Option de detection des OS, services et versions logiciels"
-    echo "3. Planification des scans"
+    echo "3. Planification des scans [WIP]"
     echo "4. Quit"
     read -p "Choisissez une option : " choix 
 
     case $choix in
-        1) clear
-        echo "1. WIP"
-        echo "2. WIP"
-        echo "3. WIP"
-        echo "4. WIP"
-        read -p "Choisissez une option : " choix_1
-        case $choix_1 in
-            1) fast_scan #echo "WIP"
-            ;;
-            2) echo "WIP"
-            ;;
-            3) echo "WIP"
-            ;;
-            4)  echo "WIP" #Back
-            ;;
-            *) echo "Choix invalide. Essayez de nouveau.";;
-        esac;;
 
-        2)read -p "Choisissez une option : " choix_1
-        case $choix_1 in
-            1) echo "WIP"
-            ;;
-            2) echo "WIP"
-            ;;
-            3) echo "WIP"
-            ;;
-            4)  echo "WIP" #Back
-            ;;
-            *) echo "Choix invalide. Essayez de nouveau.";;
-        esac;;
+        1) 
+        sous_menu_scans
+        ;;
 
-        3)read -p "Choisissez une option : " choix_1
-        case $choix_1 in
-            1) echo "WIP"
-            ;;
-            2) echo "WIP"
-            ;;
-            3) echo "WIP"
-            ;;
-            4)  echo "WIP" #Back
-            ;;
-            *) echo "Choix invalide. Essayez de nouveau.";;
-        esac;;
+        2)
+        sous_menu_detection
+        ;;
+
+        3)
+        sous_menu_planification
+        ;;
 
         4) 
         echo "Sortie du programme."
         sleep 1
         clear
-        exit;;
+        exit
+        ;;
 
-        *) echo "Choix invalide. Essayez de nouveau.";;
+        *) 
+        clear
+        echo "Choix invalide. Essayez de nouveau."
+        ;;
     esac
 done
-        
+
